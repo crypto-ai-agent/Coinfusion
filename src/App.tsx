c26958662d203be576164ff3766f265111a568d3
SBAI101 <shayldonb@gmail.com>
2025-01-30 01:26:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ export default function App() {
         <Route path="/education/:category/:id" element={<ContentViewer />} />
         <Route 
           path="/quiz/:quizId" 
-          element={<QuizTaking onComplete={handleQuizComplete} />} 
+          <QuizTaking quizId={quizId} onComplete={handleQuizComplete} /> 
         />
         <Route path="/rankings" element={<Rankings />} />
         <Route path="/news" element={<NewsPage />} />
