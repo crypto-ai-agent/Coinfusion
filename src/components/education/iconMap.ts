5ce86c9d36cca9af3e3ee2ca6d1202981d64a7e1
SBAI101 <shayldonb@gmail.com>
2025-02-09 01:43:15 +0100

    Delete src directory

--- src/components/education/iconMap.ts
+++
@@ -1,8 +0,0 @@
-import { BookOpen, Shield, TrendingUp, Lightbulb } from "lucide-react";
-
-export const iconMap = {
-  'Crypto Basics': BookOpen,
-  'Security': Shield,
-  'Investment': TrendingUp,
-  'Advanced Topics': Lightbulb,
-} as const;
--- src/components/ui/sidebar/index.tsx
+++
@@ -1,8 +0,0 @@
-export * from "./components/Sidebar";
-export * from "./components/SidebarContent";
-export * from "./components/SidebarGroup";
-export * from "./components/SidebarMenu";
-export * from "./components/SidebarProvider";
-export { useSidebar } from "./context";
-export * from "./types";
-export * from "./variants";
